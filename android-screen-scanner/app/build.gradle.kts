d899382a0cc277521c9e2e4f62610d2bd97d4200
minhaz8sa-hash <minhaz8sa@gmail.com>
2026-09-06 13:21:24 +0600

    Build token-free scanner v1.4.1

--- android-screen-scanner/app/build.gradle.kts
+++ android-screen-scanner/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.tradetracklive.scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.4.1"
     }
 }
